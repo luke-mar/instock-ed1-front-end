1ee983c2b03ee724810750b0c1a4d888103366b3
Natasha Anglade <nangl003@fiu.edu>
2023-04-26 10:06:35 -0400

    Saving changes made to Header component to then get recent develop branch code (needed for styling. Partials files' data is outdated).

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
-      <header>
-        Hello World!
-      </header>
+      <Header />
     </>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import logo from "../assets/Logo/InStock-Logo_1x.png";
+import "./Header.scss";
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="header__logo">
+                <img src={logo} alt="InStock Logo at original size"></img>
+            </div>
+            <nav className="header__nav"> 
+                <ul className="header__navList">
+                    <li className="header__navItem"><a className="header__navItemLink" href="/">Warehouses</a></li>
+                    <li className="header__navItem"><a className="header__navItemLink" href="/">Inventory</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
