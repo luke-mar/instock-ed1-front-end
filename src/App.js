483bb706015e62625d4e75a6ec41daf81f6648ff
Gennesis Baez <gennesis.baez1@gmail.com>
2023-04-27 15:19:07 -0400

    warehouse list component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import WarehouseList from './components/WarehouseList';
 
 function App() {
   return (
-    <>
+    <body>
       <header>
         Hello World!
       </header>
-    </>
+      <main>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/warehouses" element={<WarehouseList />} />
+          </Routes>
+        </BrowserRouter>
+        </main>
+    </body>
+  
   );
 }
 
