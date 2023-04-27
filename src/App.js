4a94ce4596ddd4a1f7ce9553d540c5fb3c96b949
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-04-27 15:39:51 -0400

    deleted the routes path

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './App.css';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import WarehouseList from './components/WarehouseList';
-import Header from './components/Header';
-import TestDelete from './components/TesteDelete/TestDelete';
-
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import WarehouseList from "./components/WarehouseList";
+import Header from "./components/Header";
+import TestDelete from "./components/TesteDelete/TestDelete";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Route path="/warehouses" element={<WarehouseList />} />
-        {/* <DeleteWarehouses/> */}
-     <TestDelete/>
-    </>
-
-  );
+    return (
+        <>
+            <Header />
+            <Routes>
+              <Route path="/warehouses" element={<WarehouseList />} />
+            </Routes>
+            {/* <DeleteWarehouses/> */}
+            <TestDelete />
+        </>
+    );
 }
 
 export default App;
