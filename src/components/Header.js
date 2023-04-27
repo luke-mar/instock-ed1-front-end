9654d04a785f450d581aa9ae1f1af6bf97db26fc
Natasha Anglade <nangl003@fiu.edu>
2023-04-27 13:28:35 -0400

    Finalized styling for header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/Logo/InStock-Logo_1x.png";
 import "./Header.scss";
 import "../styles/partials/_globals.scss";
@@ -6,13 +7,13 @@ function Header() {
     return (
         <header className="header">
             <div className="header__logo">
-                <img src={logo} alt="InStock Logo at original size"></img>
+                <Link to={"/"}>
+                    <img src={logo} alt="InStock Logo at original size"></img>
+                </Link>
             </div>
             <nav className="header__nav"> 
-                <ul className="header__navList">
-                    <li className="header__navItem"><a className="header__navItemLink" href="/">Warehouses</a></li>
-                    <li className="header__navItem"><a className="header__navItemLink" href="/">Inventory</a></li>
-                </ul>
+                <Link className="header__navItemLink" to={"/"}>Warehouses</Link>
+                <Link className="header__navItemLink" to={"/"}>Inventory</Link>
             </nav>
         </header>
     )
