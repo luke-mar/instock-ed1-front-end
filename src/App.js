d278366c8994b7be3bc780492d944d34c3c1ea35
Lucas <lukemar112@gmail.com>
2023-04-27 16:08:40 -0400

    making progress

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import TestDelete from './components/TesteDelete/TestDelete';
+import SingleWarehouseInventory from './components/SingleWarehouseInventory/SingleWarehouseInventory';
 
 function App() {
+
   return (
     <>
-      <header>
-        {/* <DeleteWarehouses/> */}
-     <TestDelete/>
-      </header>
+      <SingleWarehouseInventory />
     </>
   );
 }
---
+++ src/components/SingleWarehouseInventory/SingleWarehouseInventory.js
@@ -0,0 +1,52 @@
+import "./SingleWarehouseInventory.scss"
+import edit from '../../assets/Icons/edit-24px.svg'
+import deleteImg from '../../assets/Icons/delete_outline-24px.svg'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from "axios";
+
+function SingleWarehouseInventory(props) {
+  const [inventoryList, setInventoryList] = useState([]);
+  
+  useEffect(() => {
+    axios.get(`http://localhost:8080/inventories`)
+      .then((response) => {
+        if(response.data){
+        setInventoryList(response.data);
+        }
+      });
+    }, []);
+    console.log(inventoryList);
+    return (
+        <>
+          <div className="inventory-titles">
+            <p className="inventory-titles__text">INVENTORY ITEM</p>
+            <p className="inventory-titles__text">CATEGORY</p>
+            <p className="inventory-titles__text">STATUS</p>
+            <p className="inventory-titles__text">QUANTITY</p>
+            <p className="inventory-titles__last-text">ACTIONS</p>
+          </div>
+        
+        <div className="column">
+            {
+                inventoryList.filter(item => item.warehouse_name === 'Manhattan').map (invent =>{
+                    return (
+                        <div className="inventory-item">
+                        <p className="inventory-item__name">{invent.item_name} </p>
+                        <p className="inventory-item__category">{invent.category}</p>
+                        <p className="inventory-item__stock">{invent.status}</p>
+                        <p className="inventory-item__quantity">{invent.quantity}</p>
+                        <div className="inventory-item__icons">
+                            <img src={deleteImg}></img>
+                            <img src={edit}></img>
+                        </div>
+                        </div>
+                    );
+                })
+            }
+        </div> 
+        </>
+    );
+}
+
+export default SingleWarehouseInventory;
