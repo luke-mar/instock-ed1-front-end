b38387d0f58b33c7728ac38f648f4a4958949df5
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-04-27 11:10:40 -0400

    header format is being worked on

--- src/components/WarehouseDetails/WarehouseDetails.js
+++ src/components/WarehouseDetails/WarehouseDetails.js
@@ -1,1 +1,23 @@
 import "./WarehouseDetails.scss";
+
+const WarehouseDetails = ()=>{
+    return(
+        <section className="warehouseDetails">
+            <div className="warehouseDetails__header-container">
+                <div>
+                    <button>Back</button>
+                    <h3>Warehouse Location</h3>
+                </div>
+                <div>
+                    <button>edit</button>
+                </div>
+            </div>
+            <div className="warehouseDetails__description-container">Description Container</div>
+            <div className="warehouseDetails__category-container">Subcategory container</div>
+            <div className="warehouseDetails__inventory-container">Map container</div>
+        </section>
+    )
+
+}
+
+export default WarehouseDetails;
