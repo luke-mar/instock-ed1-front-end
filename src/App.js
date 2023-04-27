b743315d35706ff41d3ed597e52a7377c335f9b3
Gennesis Baez <gennesis.baez1@gmail.com>
2023-04-27 17:23:52 -0400

    combining TestDelete and Warehouse List component. handler click for deleet warehouse is finished.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
               <Route path="/warehouses" element={<WarehouseList />} />
             </Routes>
             {/* <DeleteWarehouses/> */}
-            <TestDelete />
         </>
     );
 }
--- src/components/DeleteWarehouse/DeleteWarehouse.js
+++ src/components/DeleteWarehouse/DeleteWarehouse.js
@@ -22,12 +22,14 @@ const handleClick = e => {
 }
 
 
-function DeleteWarehouse({ onclose }) {
+function DeleteWarehouse({ onclose, warehouseToDelete }) {
+
+    
     return (
         <div className="delete-container">
             <img onClick={onclose} className="delete-container__close-image" src={close} alt="close" />
-            <h2 className="delete-container__title">Delete Washington warehouse?</h2>
-            <p className="delete-container__text">Please confirm that you'd like to delete the Washington from the list of warehouses. You won't be able to undo this action</p>
+            <h2 className="delete-container__title">Delete {warehouseToDelete.warehouse_name} warehouse?</h2>
+            <p className="delete-container__text">Please confirm that you'd like to delete the {warehouseToDelete.warehouse_name}  from the list of warehouses. You won't be able to undo this action</p>
             <div className="delete-container__buttons">
                 <button onClick={onclose} className="delete-container__button1" >
                     Cancel
