1745324c0646e7e48a5051c271b495f3971eb9a9
Gennesis Baez <gennesis.baez1@gmail.com>
2023-04-28 17:11:13 -0400

    functionality for add inventory

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import WarehouseList from "./components/WarehouseList";
+import InventoryList from "./components/InventoryList";
 import Header from "./components/Header";
 import TestDelete from "./components/TesteDelete/TestDelete";
 
@@ -12,6 +13,7 @@ function App() {
             <Header />
             <Routes>
               <Route path="/warehouses" element={<WarehouseList />} />
+              <Route path="/inventories" element={<InventoryList />} />
             </Routes>
             {/* <DeleteWarehouses/> */}
         </>
