a552a4b50abb7882314ded58a70a2198810af14a
Natasha Anglade <nangl003@fiu.edu>
2023-04-28 11:56:47 -0400

    Working on styling of component.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import TestDelete from './components/TesteDelete/TestDelete';
+import AddWarehouse from './components/AddWarehouse/AddWarehouse';
 
 function App() {
   return (
     <>
       <header>
         {/* <DeleteWarehouses/> */}
+        < AddWarehouse />
      <TestDelete/>
       </header>
     </>
--- src/components/AddWarehouse/AddWarehouse.js
+++ src/components/AddWarehouse/AddWarehouse.js
@@ -1,55 +1,82 @@
 import "./AddWarehouse.scss";
-import { useRef } from "react";
+import { useState, useRef } from "react";
 import axios from "axios";
 
 function AddWarehouse() {
-    const onClick = "";
+    const formRef = useRef();
+    const [warehouses, setWarehouses] = useState();
+
+    const addWarehouse = (e) => {
+        e.preventDefault();
+        const warehouse_name = formRef.current.warehouseName.value;
+        const address = formRef.current.streetAddress.value;
+        const city = formRef.current.city.value;
+        const country = formRef.current.country.value;
+        const contact_name = formRef.current.contactName.value;
+        const contact_position = formRef.current.position.value;
+        const contact_phone = formRef.current.phoneNumber.value;
+        const contact_email = formRef.current.email.value;
+
+        axios.post("http://localhost:8080/warehouses/", {warehouse_name, address, city, country, contact_name, contact_position,
+        contact_phone, contact_email}).then((response) => {
+            setWarehouses(response.data)
+            console.log(response.data)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    };
 
     return (
-        <section>
-            <h1>
+        <section className="warehouse">
+            <h1 className="warehouse__h1">
                 Add New Warehouse
             </h1>
-            <hr/>
-            <p>Warehouse Details</p>
-            <form>
-                <label className="">
-                    Warehouse Name
-                </label>
-                <input className="" type="text" placeholder="Warehouse Name"></input>
-                <label className="">
-                    Street Address
-                </label>
-                <input className="" type="text" placeholder="Street Address"></input>
-                <label className="">
-                    City
-                </label>
-                <input className="" type="text" placeholder="City"></input>
-                <label className="">
-                    Country
-                </label>
-                <input className="" type="text" placeholder="Country"></input>
-
+            <p className="warehouse__header">Warehouse Details</p>
+            <form className="warehouse__form" onSubmit={addWarehouse} ref={formRef}>
+                <section className="warehouse__formContactDetails">
+                    <label className="warehouse__formLabel">
+                        Warehouse Name
+                    </label>
+                    <input className="warehouse__formInput" type="text" name="warehouseName" placeholder="Warehouse Name"></input>
+                    <label className="warehouse__formLabel">
+                        Street Address
+                    </label>
+                    <input className="warehouse__formInput" type="text" name="streetAddress" placeholder="Street Address"></input>
+                    <label className="warehouse__formLabel">
+                        City
+                    </label>
+                    <input className="warehouse__formInput" type="text" name="city" placeholder="City"></input>
+                    <label className="warehouse__formLabel">
+                        Country
+                    </label>
+                    <input className="warehouse__formInput" type="text" name="country" placeholder="Country"></input>
+                </section>
                 <hr/>
+                <section className="warehouse__formContactDetails">
+                    <p className="warehouse__header">Contact Details</p>
+                    <label className="warehouse__formLabel">
+                        Contact Name
+                    </label>
+                    <input className="warehouse__formInput" type="text" name="contactName" placeholder="Contact Name"></input>
+                    <label className="warehouse__formLabel">
+                        Position
+                    </label>
+                    <input className="warehouse__formInput" type="text" name="position" placeholder="Position"></input>                
+                    <label className="warehouse__formLabel">
+                        Phone Number
+                    </label>
+                    <input className="warehouse__formInput" type="text" name="phoneNumber" placeholder="Phone Number"></input>                
+                    <label className="warehouse__formLabel">
+                        Email
+                    </label>
+                    <input className="warehouse__formInput" type="text" name="email" placeholder="Email"></input>
 
-                <p>Contact Details</p>
-                <label className="">
-                    Contact Name
-                </label>
-                <input className="" type="text" placeholder="Contact Name"></input>
-                <label className="">
-                    Position
-                </label>
-                <input className="" type="text" placeholder="Position"></input>                <label className="">
-                    Phone Number
-                </label>
-                <input className="" type="text" placeholder="Phone Number"></input>                <label className="">
-                    Email
-                </label>
-                <input className="" type="text" placeholder="Email"></input>
-
-                <button type="button">Cancel</button>
-                <button type="submit">+ Add Warehouse</button>
+                    <div className="warehouse__formButtons">
+                        <button type="button">Cancel</button>
+                        <button type="submit">+ Add Warehouse</button>
+                    </div>
+                </section>
             </form>
         </section>
     )
